e479e5d86828501fc60ae9c5b5c2448b1ff70c00
junioraraujo.ads04 <junioraraujo.ads04@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-05 17:23:42 +0000

    Persistência da classe usuario

--- biblioteca-eeep/src/fvsosp/usuario/Usuario.java
+++ biblioteca-eeep/src/fvsosp/usuario/Usuario.java
@@ -1,8 +1,64 @@
 package fvsosp.usuario;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue
     private int id;
+    
+    @Column(length=50,nullable=true)
     private String login;
+    
+    @Column(length=50,nullable=true)
     private String senha;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
