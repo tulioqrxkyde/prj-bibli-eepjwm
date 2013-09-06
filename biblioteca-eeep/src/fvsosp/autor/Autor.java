73d54f5f0fbbbf2e9cb5cd6a2de60b8c83ab5bc7
Marcos Ellys Rocha Honorato <marcos.ellys@gmail.com>
2013-09-06 02:52:35 +0000

    Classe sessao do pacote fvsosp.sessao e classe autor do pacote fvsosp.autor (devidamente mapeadas).

--- biblioteca-eeep/src/fvsosp/autor/Autor.java
+++ biblioteca-eeep/src/fvsosp/autor/Autor.java
@@ -2,10 +2,21 @@ package fvsosp.autor;
 
 import fvsosp.acervo.Acervo;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.swing.text.html.parser.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name="aluno")
 public class Autor {
 
+    @Id
+    @GeneratedValue
     private int id;
+    
     private String nome;
     private String sobreOAutor;
     private List<Acervo> acervos;
