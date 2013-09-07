4622e6d31aa7cb7bd7d43fa00686fc61c3ff1679
tulio.xcrtf <tulio.xcrtf@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-07 12:58:34 +0000

    Mapeamento das Classes Biblioteca, Idioma e Emprestimo. Adição dos métodos gets e sets. Tamanho do campo Nome da Cidade alterado para 62 ( Valor Máximo Mundial ).

--- trunk/biblioteca-eeep/src/fvsosp/biblioteca/Biblioteca.java
+++ trunk/biblioteca-eeep/src/fvsosp/biblioteca/Biblioteca.java
@@ -2,10 +2,48 @@ package fvsosp.biblioteca;
 
 import fvsosp.acervo.Acervo;
 import java.util.List;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "Biblioteca")
 public class Biblioteca {
 
+    @Id
     private int idBiblioteca;
+    
+    @Column(length = 70, nullable = true)
     private String descricao;
+    
+    @OneToMany(mappedBy = "idAcervo")
     private List<Acervo> acervos;
+
+    /*** @retorna o ID da Biblioteca ***/
+    public int getIdBiblioteca() {
+        return idBiblioteca;
+    }
+
+    /*** @seta o ID da Biblioteca ***/
+    public void setIdBiblioteca(int idBiblioteca) {
+        this.idBiblioteca = idBiblioteca;
+    }
+
+    /*** @retorna a descricao ***/
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /*** @seta o nome do Idioma ***/
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /*** @retorna uma Lista de Acervos ***/
+    public List<Acervo> getAcervos() {
+        return acervos;
+    }
+
+    /*** @seta e copia uma lista de Acervos ***/
+    public void setAcervos(List<Acervo> acervos) {
+        this.acervos = acervos;
+    }
 }
--- trunk/biblioteca-eeep/src/fvsosp/emprestimo/Emprestimo.java
+++ trunk/biblioteca-eeep/src/fvsosp/emprestimo/Emprestimo.java
@@ -2,14 +2,76 @@ package fvsosp.emprestimo;
 
 import fvsosp.acervo.Acervo;
 import fvsosp.leitor.Leitor;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "Emprestimo")
 public class Emprestimo {
 
+    @Id
+    @GeneratedValue
     private int idEmprestimo;
-    private Date dataEmrepstimo;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dataEmprestimo;
+
+    @Temporal(TemporalType.DATE)
     private Date dataDevolucao;
+
+    @OneToMany(mappedBy = "idAcervo")
     private List<Acervo> acervos;
+
+    @OneToOne(mappedBy = "idLeitor")
     private Leitor leitor;
+
+    /*** @retorna o id do Empréstimo ***/
+    public int getIdEmprestimo() {
+        return idEmprestimo;
+    }
+
+    /*** @seta o id do Empréstimo ***/
+    public void setIdEmprestimo(int idEmprestimo) {
+        this.idEmprestimo = idEmprestimo;
+    }
+
+    /*** @retorna a Data do Empréstimo ***/
+    public Date getDataEmrepstimo() {
+        return dataEmprestimo;
+    }
+
+    /*** @seta a Data do Empréstimo ***/
+    public void setDataEmrepstimo(Date dataEmrepstimo) {
+        this.dataEmprestimo = dataEmrepstimo;
+    }
+
+    /*** @retorna a Data de Devolução ***/
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    /*** @seta a Data de Devolução ***/
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    /*** @retorna a lista de Acervos ***/
+    public List<Acervo> getAcervos() {
+        return acervos;
+    }
+
+    /*** @seta e copia a lista de Acervos recebida para a lista de Acervos da Classe ***/
+    public void setAcervos(List<Acervo> acervos) {
+        this.acervos = acervos;
+    }
+
+    /*** @retorna o Leitor ***/
+    public Leitor getLeitor() {
+        return leitor;
+    }
+
+    /*** @seta o Leitor ***/
+    public void setLeitor(Leitor leitor) {
+        this.leitor = leitor;
+    }
 }
