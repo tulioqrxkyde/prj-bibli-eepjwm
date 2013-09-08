83ded510342de9df2f8409c43f281a32fb8ad037
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-08 19:22:09 +0000

    Adição da classe GenricDAO, e UsuarioDAO

---
+++ biblioteca-eeep/src/fvsosp/util/GeneicDAO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fvsosp.util;
+
+import fvsosp.usuario.Usuario;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+@SuppressWarnings("unchecked")
+public abstract class GeneicDAO<T> {
+
+    private Session sessao;
+    private Transaction transacao;
+
+    public void salvar(T entity) {
+        try {
+            this.sessao = HibernateUtil.getSessionFactory().openSession();
+            this.transacao = sessao.beginTransaction();
+            this.sessao.save(entity);
+            this.transacao.commit();
+        } catch (HibernateException e) {
+            System.out.println("Não foi possível inserir " + entity.getClass()
+                    + ". Erro: " + e.getMessage());
+        } finally {
+            sessao.close();
+            
+        }
+    }
+    
+    public void atualizar(T entity) {
+        try {
+            this.sessao = HibernateUtil.getSessionFactory().openSession();
+            this.transacao = sessao.beginTransaction();
+            this.sessao.update(entity);
+            this.transacao.commit();
+        } catch (HibernateException e) {
+            System.out.println("Não foi possível atualizar " + entity.getClass()
+                    + ". Erro: " + e.getMessage());
+        } finally {
+            sessao.close();
+            
+        }
+    }
+    
+    public void remover(T entity) {
+        try {
+            this.sessao = HibernateUtil.getSessionFactory().openSession();
+            this.transacao = sessao.beginTransaction();
+            this.sessao.delete(entity);
+            this.transacao.commit();
+        } catch (HibernateException e) {
+            System.out.println("Não foi possível atualizar " + entity.getClass()
+                    + ". Erro: " + e.getMessage());
+        } finally {
+            sessao.close();
+            
+        }
+    }
+  
+}
--- biblioteca-eeep/src/fvsosp/util/Principal.java
+++ biblioteca-eeep/src/fvsosp/util/Principal.java
@@ -1,5 +1,8 @@
 package fvsosp.util;
 
+import fvsosp.usuario.Usuario;
+import fvsosp.usuario.UsuarioDAO;
+import java.util.List;
 import org.hibernate.Session;
 
 /**
@@ -7,10 +10,15 @@ import org.hibernate.Session;
  * @author Pedro Saraiva
  */
 public class Principal {
-    
+
     public static void main(String[] args) {
-        Session sessao = 
-                HibernateUtil.getSessionFactory().openSession(); 
+        Usuario u = new Usuario();
+        UsuarioDAO dao = new UsuarioDAO();
+        
+        u.setLogin("c");
+        u.setSenha("c");
+        
+        dao.salvar(u);
+        
     }
-    
 }
