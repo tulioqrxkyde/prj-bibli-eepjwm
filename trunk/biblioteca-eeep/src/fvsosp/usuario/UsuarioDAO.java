21f11efb950db028591837cb7df9868205a40868
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-08 19:22:09 +0000

    Adição da classe GenricDAO, e UsuarioDAO

---
+++ trunk/biblioteca-eeep/src/fvsosp/usuario/UsuarioDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fvsosp.usuario;
+
+import fvsosp.util.GeneicDAO;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+public class UsuarioDAO extends GeneicDAO<Usuario> {
+    
+}
