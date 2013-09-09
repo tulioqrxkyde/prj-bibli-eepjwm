2fc1d268a5dbd38d22456282a85e23aa31950158
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-09 13:52:34 +0000

    método pesquisarDescricao em idiomaDAO, e concertado método pesquisarSobreOAutor em AutoDAO

--- trunk/biblioteca-eeep/src/fvsosp/idioma/IdiomaDAO.java
+++ trunk/biblioteca-eeep/src/fvsosp/idioma/IdiomaDAO.java
@@ -5,6 +5,12 @@
 package fvsosp.idioma;
 
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -14,5 +20,23 @@ public class IdiomaDAO extends GenericDAO<Idioma>{
     
     public IdiomaDAO(){
         super(Idioma.class);
+    }
+    
+    public List<Idioma> procuraDescricao(String descricao) {
+        List<Idioma> idiomas = null;
+
+        try {
+
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+
+            idiomas = (List<Idioma>) getSessao().createCriteria(Idioma.class).
+                    add(Restrictions.like("descricao", descricao, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("descricao")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por Descrição: " + e.getMessage());
+        }
+        return idiomas;
     }
 }
