48a99662873c146a5f8cf2928a88090447f1729b
tulio.xcrtf <tulio.xcrtf@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-09 02:19:29 +0000

    Adição dos métodos de pesquisa e de autenticação de usuarios.

--- trunk/biblioteca-eeep/src/fvsosp/usuario/UsuarioDAO.java
+++ trunk/biblioteca-eeep/src/fvsosp/usuario/UsuarioDAO.java
@@ -5,13 +5,46 @@
 package fvsosp.usuario;
 
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import org.hibernate.*;
+
 /**
  *
  * @author Pedro Saraiva
  */
 public class UsuarioDAO extends GenericDAO<Usuario>{
+
+    private Session sessao;
+    private Transaction transacao;
     
     public UsuarioDAO(){
         super(Usuario.class);
+    }
+    
+    public String pesquisarLogin(String login) {
+        String name = "";
+        try {
+            this.sessao = HibernateUtil.getSessionFactory().openSession();
+            this.transacao = sessao.beginTransaction();
+            Query loginFind = sessao.createQuery("select idUsuario from Usuario where login = ?");
+            loginFind.setParameter(0, login);
+            name = loginFind.list().toString();
+        } catch (Throwable t) {
+            System.out.println("Não foi possível localizar o login.");
+        }
+        return name;
+    }
+
+    public boolean autenticaLoginSenha(String login, String senha) {
+        this.sessao = HibernateUtil.getSessionFactory().openSession();
+        this.transacao = sessao.beginTransaction();
+        Query loginFind = sessao.createQuery("from Usuario as usr where usr.login = :procurarLogin and usr.senha = :procurarSenha");
+        loginFind.setParameter("procurarLogin", login);
+        loginFind.setParameter("procurarSenha", senha);
+        if (!loginFind.list().isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- trunk/biblioteca-eeep/src/fvsosp/util/Principal.java
+++ trunk/biblioteca-eeep/src/fvsosp/util/Principal.java
@@ -15,14 +15,26 @@ public class Principal {
     public static void main(String[] args) {
         Usuario u = new Usuario();
         UsuarioDAO dao = new UsuarioDAO();
+        
         //        u.setLogin("c");
         //        u.setSenha("c");
         //
         //        dao.salvar(u);
+        
         List<Usuario> listar = dao.listar();
         for (int i = 0; i < dao.listar().size(); i++) {
             JOptionPane.showMessageDialog(null, dao.listar().get(i).getLogin());
         }
+        
+        // Modelo de Autenticação 
+        if(dao.autenticaLoginSenha("c", (String) u.md5("c")))
+            System.out.println("Sucesso");
+        else
+            System.out.println("Insucesso");
+      
+        /* Exemplo de pesquisa através do Login */ 
+         System.out.println("Login: "+dao.pesquisarLogin(u.getLogin()));
+        //*/
         
     }
 }
