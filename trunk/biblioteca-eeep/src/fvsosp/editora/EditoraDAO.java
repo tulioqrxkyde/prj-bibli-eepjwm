8522b36644ff441527a7678432df796f095cfc24
junioraraujo.ads04 <junioraraujo.ads04@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-09 13:12:38 +0000

    Método que passado uma descrição retorna uma lista de editoras

--- trunk/biblioteca-eeep/src/fvsosp/editora/EditoraDAO.java
+++ trunk/biblioteca-eeep/src/fvsosp/editora/EditoraDAO.java
@@ -4,15 +4,40 @@
  */
 package fvsosp.editora;
 
+import fvsosp.tipoitem.TipoItem;
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author Oziel
  */
-public class EditoraDAO extends GenericDAO<Editora>{
-    
-    public EditoraDAO(){
+public class EditoraDAO extends GenericDAO<Editora> {
+
+    public EditoraDAO() {
         super(Editora.class);
+    }
+
+    public List<Editora> procuraDescricao(String descricao) {
+        List<Editora> editoras = null;
+
+        try {
+
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+
+            editoras = (List<Editora>) getSessao().createCriteria(Editora.class).
+                    add(Restrictions.like("descricao", descricao, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("descricao")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por Descrição: " + e.getMessage());
+        }
+        return editoras;
     }
 }
