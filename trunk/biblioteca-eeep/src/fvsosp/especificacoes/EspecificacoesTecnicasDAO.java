f02bde90b8cadc79a8bdf4d9a05a80fb1ceb05bf
marcos.ellys <marcos.ellys@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-09 16:46:33 +0000

    Metodo (pesquisarAcervo) na classe especificacoestecnicasDAO criado e em grupoLeitoresDAO método (pesquisaDescricao) criado

--- trunk/biblioteca-eeep/src/fvsosp/especificacoes/EspecificacoesTecnicasDAO.java
+++ trunk/biblioteca-eeep/src/fvsosp/especificacoes/EspecificacoesTecnicasDAO.java
@@ -4,7 +4,14 @@
  */
 package fvsosp.especificacoes;
 
+import fvsosp.acervo.Acervo;
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -15,4 +22,26 @@ public class EspecificacoesTecnicasDAO extends GenericDAO<EspecificacoesTecnicas
     public EspecificacoesTecnicasDAO(){
         super(EspecificacoesTecnicas.class);
     }
+
+  /* Metódo da Pesquisa criado por Marcos Ellys */  
+    
+public List<Acervo> pesquinaAcervo(String acervo) {
+        List<Acervo> acervos = null;
+
+        try {
+
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+
+            acervos = (List<Acervo>) getSessao().createCriteria(Acervo.class).
+                    add(Restrictions.like("acervo", acervo, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("acervo")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por Acervo: " + e.getMessage());
+        }
+        return acervos;
+    }
+
+
 }
