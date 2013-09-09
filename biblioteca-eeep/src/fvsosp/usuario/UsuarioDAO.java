96d3e71bbce3e09f8b1f136a6906c21247a14b0e
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-09 11:53:17 +0000

    modificado o método autenticaLoginSenha para receber um Usuário. Modificado o método pesquisarLogin para retornar um Usuario

--- biblioteca-eeep/src/fvsosp/usuario/UsuarioDAO.java
+++ biblioteca-eeep/src/fvsosp/usuario/UsuarioDAO.java
@@ -6,7 +6,7 @@ package fvsosp.usuario;
 
 import fvsosp.util.GenericDAO;
 import fvsosp.util.HibernateUtil;
-import org.hibernate.*;
+import org.hibernate.Query;
 
 /**
  *
@@ -14,33 +14,38 @@ import org.hibernate.*;
  */
 public class UsuarioDAO extends GenericDAO<Usuario>{
 
-    private Session sessao;
-    private Transaction transacao;
-    
     public UsuarioDAO(){
         super(Usuario.class);
     }
     
-    public String pesquisarLogin(String login) {
-        String name = "";
+    /*
+     * Recebe um login por parâmetro e procura
+     * se encontrar retorna o usuário
+     */
+    public Usuario pesquisarLogin(String login) {
+        Usuario usuario=null;
         try {
-            this.sessao = HibernateUtil.getSessionFactory().openSession();
-            this.transacao = sessao.beginTransaction();
-            Query loginFind = sessao.createQuery("select idUsuario from Usuario where login = ?");
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(this.getSessao().beginTransaction());
+            Query loginFind = getSessao().createQuery("from Usuario where login = ?");
             loginFind.setParameter(0, login);
-            name = loginFind.list().toString();
+            usuario = (Usuario) loginFind.list().get(0);
         } catch (Throwable t) {
             System.out.println("Não foi possível localizar o login.");
         }
-        return name;
+        return usuario;
     }
 
-    public boolean autenticaLoginSenha(String login, String senha) {
-        this.sessao = HibernateUtil.getSessionFactory().openSession();
-        this.transacao = sessao.beginTransaction();
-        Query loginFind = sessao.createQuery("from Usuario as usr where usr.login = :procurarLogin and usr.senha = :procurarSenha");
-        loginFind.setParameter("procurarLogin", login);
-        loginFind.setParameter("procurarSenha", senha);
+    /*
+     * Recebe um usuário e verifica o seu login e senha estão corretos
+     * caso estejam ele retorna true, caso não retorna false
+     */
+    public boolean autenticaLoginSenha(Usuario usuario) {
+        this.setSessao(HibernateUtil.getSessionFactory().openSession());
+        this.setTransacao(this.getSessao().beginTransaction());
+        Query loginFind = this.getSessao().createQuery("from Usuario as usr where usr.login = :procurarLogin and usr.senha = :procurarSenha");
+        loginFind.setParameter("procurarLogin", usuario.getLogin());
+        loginFind.setParameter("procurarSenha", usuario.getSenha());
         if (!loginFind.list().isEmpty()) {
             return true;
         } else {
