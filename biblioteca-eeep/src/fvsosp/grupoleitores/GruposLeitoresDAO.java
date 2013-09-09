ca123cb5110912976ea7b28b2052bfa26cb81382
Marcos Ellys Rocha Honorato <marcos.ellys@gmail.com>
2013-09-09 16:46:33 +0000

    Metodo (pesquisarAcervo) na classe especificacoestecnicasDAO criado e em grupoLeitoresDAO método (pesquisaDescricao) criado

--- biblioteca-eeep/src/fvsosp/grupoleitores/GruposLeitoresDAO.java
+++ biblioteca-eeep/src/fvsosp/grupoleitores/GruposLeitoresDAO.java
@@ -4,7 +4,14 @@
  */
 package fvsosp.grupoleitores;
 
+import fvsosp.editora.Editora;
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -15,4 +22,26 @@ public class GruposLeitoresDAO extends GenericDAO<GruposLeitores>{
     public GruposLeitoresDAO(){
         super(GruposLeitores.class);
     }
+    
+      /* Metódo da Pesquisa criado por Marcos Ellys */  
+    
+  public List<Editora> pesquisaDescricao(String descricao) {
+        List<Editora> descricaoPesquisa = null;
+
+        try {
+
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+
+            descricaoPesquisa = (List<Editora>) getSessao().createCriteria(Editora.class).
+                    add(Restrictions.like("descricao", descricao, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("descricao")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por Descrição: " + e.getMessage());
+        }
+        return descricaoPesquisa;
+    }
+
+
 }
