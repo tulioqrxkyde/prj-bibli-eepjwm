5564a17a7da30ca2c43463922ba48921e5167792
oziel.ico <oziel.ico@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-10 03:02:39 +0000

    Implementeando a Classe BibliotecaDAO com os metodos pesquisar, e Classe CidadeDAO com os metodos pesquisarCodIbge, pesquisarDescricao, pesquisarUf.

--- trunk/biblioteca-eeep/src/fvsosp/cidade/CidadeDAO.java
+++ trunk/biblioteca-eeep/src/fvsosp/cidade/CidadeDAO.java
@@ -4,7 +4,14 @@
  */
 package fvsosp.cidade;
 
+import fvsosp.biblioteca.Biblioteca;
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
@@ -14,5 +21,55 @@ public class CidadeDAO extends GenericDAO<Cidade> {
 
     public CidadeDAO() {
         super(Cidade.class);
+    }
+
+    public List<Cidade> pesquisarCodIbge(int codibge) {
+        List<Cidade> cidades = null;
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            
+            cidades = (List<Cidade>) getSessao().createCriteria(Cidade.class).
+                    add(Restrictions.eq("codibge", codibge)).
+                    addOrder(Order.asc("nome")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por CodigoIbge: " + e.getMessage());
+        }
+        return cidades;
+    
+    }
+        
+    public List<Cidade> pesquisarDescricao(String descricao) {
+        List<Cidade> cidades = null;
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            
+            cidades = (List<Cidade>) getSessao().createCriteria(Cidade.class).
+                    add(Restrictions.like("descricao", descricao, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("descricao")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por Descrição: " + e.getMessage());
+        }
+        return cidades;
+    
+    }
+       
+        public List<Cidade> pesquisarUf(String descricao) {
+        List<Cidade> cidades = null;
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            
+            cidades = (List<Cidade>) getSessao().createCriteria(Cidade.class).
+                    add(Restrictions.like("descricao", descricao, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("descricao")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao pesquisar por UF: " + e.getMessage());
+        }
+        return cidades;
     }
 }
