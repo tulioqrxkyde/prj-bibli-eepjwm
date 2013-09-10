aa6ebd97428af4517beeb104c9a100ffefa2fbd6
oziel.ico <oziel.ico@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-10 03:02:39 +0000

    Implementeando a Classe BibliotecaDAO com os metodos pesquisar, e Classe CidadeDAO com os metodos pesquisarCodIbge, pesquisarDescricao, pesquisarUf.

--- biblioteca-eeep/src/fvsosp/biblioteca/BibliotecaDAO.java
+++ biblioteca-eeep/src/fvsosp/biblioteca/BibliotecaDAO.java
@@ -5,14 +5,43 @@
 package fvsosp.biblioteca;
 
 import fvsosp.util.GenericDAO;
+import fvsosp.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author Oziel
  */
-public class BibliotecaDAO extends GenericDAO<Biblioteca>{
-    
-    public BibliotecaDAO(){
+public class BibliotecaDAO extends GenericDAO<Biblioteca> {
+
+    public BibliotecaDAO() {
         super(BibliotecaDAO.class);
     }
+
+    public List<Biblioteca> pesquisarDescricao(String descricao) {
+        List<Biblioteca> bibliotecas = null;
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            /*
+             * pesquisa uma biblioteca por descricao
+             * usando o like, o MatchMode.ANYWHERE quer dizer
+             * que a sequência de caracteres passada na String descricao
+             * pode ser encontrada em qualquer lugar, representa o '%descricao%', ordenando
+             * de forma crescente por descicao
+             */
+            bibliotecas = (List<Biblioteca>) getSessao().createCriteria(Biblioteca.class).
+                    add(Restrictions.like("descricao", descricao, MatchMode.ANYWHERE)).
+                    addOrder(Order.asc("descricao")).list();
+
+        } catch (HibernateException e) {
+            System.out.println("Erro ao procurar por Descrição: " + e.getMessage());
+        }
+        return bibliotecas;
+    }
 }
+
