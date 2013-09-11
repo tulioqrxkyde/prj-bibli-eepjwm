75c7011918bcd220537302f260d9f72699bea79d
tulio.xcrtf <tulio.xcrtf@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-11 03:36:04 +0000

    Criação do EmprestimoRN, revisão na classe EmprestimoDAO, correção em nome de método getDataEmrepstimo para getDataEmprestimo.

---
+++ trunk/biblioteca-eeep/src/fvsosp/emprestimo/EmprestimoRN.java
@@ -0,0 +1,47 @@
+package fvsosp.emprestimo;
+
+import fvsosp.acervo.Acervo;
+import java.util.*;
+
+/**
+ *
+ * @author Túlio
+ */
+public class EmprestimoRN {
+
+    EmprestimoDAO dao = new EmprestimoDAO();
+
+    public boolean adiciona(Emprestimo em) {
+        if ((em.getIdEmprestimo() > 0) && (!em.getDataEmprestimo().toString().isEmpty()) && (em.getLeitor() != null) && (!em.getAcervos().isEmpty())) {
+            dao.adicionar(em);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remover(Emprestimo em) {
+        dao.remover(em);
+        return true;
+    }
+
+    public boolean atualizar(Emprestimo em) {
+        dao.atualizar(em);
+        return true;
+    }
+
+    public List<Emprestimo> listar() {
+        return dao.listar();
+    }
+
+    public List<Emprestimo> pesquisardataEmprestimo(Date data) {
+        return dao.pesquisardataEmprestimo(data);
+    }
+
+    public List<Emprestimo> pesquisardataDevolucao(Date data) {
+        return dao.pesquisardataEmprestimo(data);
+    }
+
+    public Acervo pesquisarEmprestimo(Emprestimo emprestimo) {
+        return dao.pesquisarEmprestimo(emprestimo);
+    }
+}
