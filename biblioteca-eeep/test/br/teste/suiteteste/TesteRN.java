8be4ea5963ebbf89f3b094d2a19a13ebfb6d609a
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-12 03:10:21 +0000

    inserido classe UsuarioRNTest, que testa todos os métodos da classe UsuarioRN

---
+++ biblioteca-eeep/test/br/teste/suiteteste/TesteRN.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.teste.suiteteste;
+
+import fvsosp.usuario.UsuarioRNTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({UsuarioRNTest.class})
+public class TesteRN {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
