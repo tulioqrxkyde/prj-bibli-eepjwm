0b5631dd8e658e3ee836f50c43cb263d8c720cc8
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-12 14:19:52 +0000

    isnerido a classe SessaoRenTest

---
+++ biblioteca-eeep/test/fvsosp/sessao/SessaoRNTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fvsosp.sessao;
+
+import fvsosp.acervo.Acervo;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+public class SessaoRNTest {
+    
+    public SessaoRNTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of adicionar method, of class SessaoRN.
+     */
+    @Test
+    public void testAdicionar() {
+        fail();
+    }
+
+    /**
+     * Test of atualizar method, of class SessaoRN.
+     */
+    @Test
+    public void testAtualizar() {
+       fail();
+    }
+
+    /**
+     * Test of remove method, of class SessaoRN.
+     */
+    @Test
+    public void testRemove() {
+        fail();
+    }
+
+    /**
+     * Test of listar method, of class SessaoRN.
+     */
+    @Test
+    public void testListar() {
+        fail();
+    }
+
+    /**
+     * Test of pesquisarDescricao method, of class SessaoRN.
+     */
+    @Test
+    public void testPesquisarDescricao() {
+        fail();
+    }
+
+    /**
+     * Test of pesquisarAcervos method, of class SessaoRN.
+     */
+    @Test
+    public void testPesquisarAcervos() {
+        fail();
+    }
+}
