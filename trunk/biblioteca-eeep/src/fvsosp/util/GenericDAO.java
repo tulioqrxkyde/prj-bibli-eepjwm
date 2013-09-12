bc1db035faa45c208c0b3fb445ef1b6422761d95
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-12 14:09:38 +0000

    alterações no teste da classe usuariorn, inserção da classe tipoitemrnteste

---
+++ trunk/biblioteca-eeep/src/fvsosp/util/GenericDAO.java
@@ -0,0 +1,145 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fvsosp.util;
+
+import fvsosp.usuario.Usuario;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+@SuppressWarnings("unchecked")
+public abstract class GenericDAO<T> {
+    
+    /*
+     * Essa classe possui operações comuns a todas as classes persistentes
+     * para utilizar-la deverá criar uma classe DAO específica para a classe
+     * persistente e extends GenericDAO<T> onde T é a classe Persistente 
+     */
+
+    private Session sessao;
+    private Transaction transacao;
+    private Class classe ;
+
+    public GenericDAO(Class classe) {
+        this.classe = classe;
+    }
+
+    public boolean adicionar(T entity) {
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            this.getSessao().save(entity);
+            this.getTransacao().commit();
+            
+        } catch (HibernateException e) {
+            System.out.println("Não foi possível inserir " + entity.getClass()
+                    + ". Erro: " + e.getMessage());
+            return false;
+        } finally {
+            getSessao().close();
+
+        }
+        return true;
+    }
+
+    public boolean atualizar(T entity) {
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            this.getSessao().update(entity);
+            this.getTransacao().commit();
+        } catch (HibernateException e) {
+            System.out.println("Não foi possível atualizar " + entity.getClass()
+                    + ". Erro: " + e.getMessage());
+            return false;
+        } finally {
+            getSessao().close();
+
+        }
+            return true;
+    }
+
+    public boolean remover(T entity) {
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            this.setTransacao(getSessao().beginTransaction());
+            this.getSessao().delete(entity);
+            this.getTransacao().commit();
+        } catch (HibernateException e) {
+            System.out.println("Não foi possível remover " + entity.getClass()
+                    + ". Erro: " + e.getMessage());
+            return false;
+        } finally {
+            getSessao().close();
+
+        }
+        return true;
+    }
+
+    public List<T> listar() {
+        List<T> lista = null;
+        try {
+            this.setSessao(HibernateUtil.getSessionFactory().openSession());
+            setTransacao(getSessao().beginTransaction());
+            lista = this.getSessao().createCriteria(classe).list();
+            sessao.close();
+        } catch (Throwable e) {
+            if (getTransacao().isActive()) {
+                getTransacao().rollback();
+            }
+            System.out.println("Erro ao listar. Erro " + e.getMessage());
+        }
+        return lista;
+    }
+    
+    /*
+     * ao passar uma chave primária
+     * ele retorna um objeto referente a chave primária
+     */
+    public T carregaChavePrimaria(int chavePrimaria){
+        try {
+            sessao = HibernateUtil.getSessionFactory().openSession();
+            Object o = sessao.load(classe, chavePrimaria);
+            return (T) o;
+        } catch (HibernateException e) {
+            System.out.println("Erro ao carregar por chave primária: "+e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * @return the sessao
+     */
+    public Session getSessao() {
+        return sessao;
+    }
+
+    /**
+     * @param sessao the sessao to set
+     */
+    public void setSessao(Session sessao) {
+        this.sessao = sessao;
+    }
+
+    /**
+     * @return the transacao
+     */
+    public Transaction getTransacao() {
+        return transacao;
+    }
+
+    /**
+     * @param transacao the transacao to set
+     */
+    public void setTransacao(Transaction transacao) {
+        this.transacao = transacao;
+    }
+}
---
+++ trunk/biblioteca-eeep/test/br/teste/suiteteste/TesteRN.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.teste.suiteteste;
+
+import fvsosp.tipoitem.TipoItemRNTest;
+import fvsosp.usuario.UsuarioRNTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({UsuarioRNTest.class,TipoItemRNTest.class})
+public class TesteRN {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
