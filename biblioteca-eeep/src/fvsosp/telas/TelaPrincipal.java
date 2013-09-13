017d295b040d5ea38e1d93f1f11756472824f3a4
adriano@fvs.edu.br <adriano@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-13 20:47:36 +0000

    trocado nome

--- biblioteca-eeep/src/fvsosp/telas/TelaPrincipal.java
+++ biblioteca-eeep/src/fvsosp/telas/TelaPrincipal.java
@@ -8,12 +8,12 @@ package fvsosp.telas;
  *
  * @author ADRIANO
  */
-public class Tela_Principal extends javax.swing.JFrame {
+public class TelaPrincipal extends javax.swing.JFrame {
 
     /**
      * Creates new form Tela_Principal
      */
-    public Tela_Principal() {
+    public TelaPrincipal() {
         initComponents();
     }
 
@@ -96,20 +96,20 @@ public class Tela_Principal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Tela_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Tela_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Tela_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tela_Principal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Tela_Principal().setVisible(true);
+                new TelaPrincipal().setVisible(true);
             }
         });
     }
