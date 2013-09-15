6a418ec714efb594da9e9c8dd7d12ab29943835d
henricky.mestre22 <henricky.mestre22@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-15 23:18:12 +0000

    Alterado design da tela de login

--- biblioteca-eeep/src/fvsosp/telas/TelaPrincipal.java
+++ biblioteca-eeep/src/fvsosp/telas/TelaPrincipal.java
@@ -69,11 +69,11 @@ public class TelaPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 707, Short.MAX_VALUE)
+            .addGap(0, 775, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 408, Short.MAX_VALUE)
+            .addGap(0, 382, Short.MAX_VALUE)
         );
 
         pack();
