e403780e5b0a0b42ee9821f9f49286f1928c947b
henricky.mestre22 <henricky.mestre22@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-17 03:10:54 +0000

    Alterado design da tela de login

--- biblioteca-eeep/src/fvsosp/telas/Login.java
+++ biblioteca-eeep/src/fvsosp/telas/Login.java
@@ -39,6 +39,7 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        canvas1 = new java.awt.Canvas();
         jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -272,6 +273,7 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btCancelar;
     private javax.swing.JButton btLogar;
+    private java.awt.Canvas canvas1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
