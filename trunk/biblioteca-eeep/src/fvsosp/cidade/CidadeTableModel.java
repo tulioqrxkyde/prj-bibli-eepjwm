54bbd1fc1b0639619e04b2b20abab39318de3fb7
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2013-09-26 15:43:21 +0000

    adicionado tablemodel para cidade, descomentado método epsquisaIBGE de cidadedao

---
+++ trunk/biblioteca-eeep/src/fvsosp/cidade/CidadeTableModel.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fvsosp.cidade;
+
+import fvsosp.autor.Autor;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ *
+ * @author Controle Avaliação
+ */
+public class CidadeTableModel extends AbstractTableModel {
+
+    private String[] nomeColunas = {"CodIBGE", "Descrição", "UF"};
+    private List<Cidade> cidades;
+
+    public CidadeTableModel() {
+        cidades = new ArrayList<Cidade>();
+    }
+
+    public CidadeTableModel(List<Cidade> lista) {
+        this();
+        this.cidades.clear();
+        this.cidades.addAll(lista);
+        super.fireTableDataChanged();
+
+    }
+
+    public int getRowCount() {
+        return cidades.size();
+        //throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+    public int getColumnCount() {
+        return nomeColunas.length;
+        //throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Cidade cidade = cidades.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return cidade.getCodIBGE();
+            case 1:
+                return cidade.getDescricao();
+            case 2:
+                return cidade.getUf();
+        }
+        return null;
+        //throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:
+                return nomeColunas[0];
+            case 1:
+                return nomeColunas[1];
+            case 2:
+                return nomeColunas[2];
+        }
+        return null;
+    }
+}
