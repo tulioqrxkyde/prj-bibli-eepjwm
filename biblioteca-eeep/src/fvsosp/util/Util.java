98013305717a38a71feb52e38fa721cf93117e80
tulio.xcrtf <tulio.xcrtf@gmail.com>
2013-12-23 06:09:49 +0000

    Novas alterações na Tela de Cadastro[ Cidades ]. Follow us ;)

--- biblioteca-eeep/src/fvsosp/util/Util.java
+++ biblioteca-eeep/src/fvsosp/util/Util.java
@@ -7,9 +7,6 @@ package fvsosp.util;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
@@ -19,73 +16,77 @@ import javax.swing.JOptionPane;
  * @author Pedro Saraiva
  */
 public class Util {
-    /** Realiza a validação do CPF. 
-     * 
-     * @param   strCPF número de CPF a ser validado 
-     * @return  true se o CPF é válido e false se não é válido 
-     */  
-    public static boolean CPF(String strCpf) {  
-        if (strCpf.equals("")) {  
-            return false;  
+
+    /**
+     * Realiza a validação do CPF.
+     *
+     * @param strCPF número de CPF a ser validado
+     * @return true se o CPF é válido e false se não é válido
+     */
+    public static boolean CPF(String strCpf) {
+        if (strCpf.equals("")) {
+            return false;
         }
-        
-        if (strCpf.equals("00000000000") || strCpf.equals("11111111111") ||  
-        strCpf.equals("22222222222") || strCpf.equals("33333333333") ||  
-        strCpf.equals("44444444444") || strCpf.equals("55555555555") ||  
-        strCpf.equals("66666666666") || strCpf.equals("77777777777") ||  
-        strCpf.equals("88888888888") || strCpf.equals("99999999999") ||  
-       (strCpf.length() != 11))  
-       return(false);  
-        
-        int d1, d2;  
-        int digito1, digito2, resto;  
-        int digitoCPF;  
-        String nDigResult;  
-  
-        d1 = d2 = 0;  
-        digito1 = digito2 = resto = 0;  
-  
-        for (int nCount = 1; nCount < strCpf.length() - 1; nCount++) {  
-            digitoCPF = Integer.valueOf(strCpf.substring(nCount - 1, nCount)).intValue();  
-  
+
+        if (strCpf.equals("00000000000") || strCpf.equals("11111111111")
+                || strCpf.equals("22222222222") || strCpf.equals("33333333333")
+                || strCpf.equals("44444444444") || strCpf.equals("55555555555")
+                || strCpf.equals("66666666666") || strCpf.equals("77777777777")
+                || strCpf.equals("88888888888") || strCpf.equals("99999999999")
+                || (strCpf.length() != 11)) {
+            return (false);
+        }
+
+        int d1, d2;
+        int digito1, digito2, resto;
+        int digitoCPF;
+        String nDigResult;
+
+        d1 = d2 = 0;
+        digito1 = digito2 = resto = 0;
+
+        for (int nCount = 1; nCount < strCpf.length() - 1; nCount++) {
+            digitoCPF = Integer.valueOf(strCpf.substring(nCount - 1, nCount)).intValue();
+
             //multiplique a ultima casa por 2 a seguinte por 3 a seguinte por 4 e assim por diante.  
-            d1 = d1 + (11 - nCount) * digitoCPF;  
-  
+            d1 = d1 + (11 - nCount) * digitoCPF;
+
             //para o segundo digito repita o procedimento incluindo o primeiro digito calculado no passo anterior.  
-            d2 = d2 + (12 - nCount) * digitoCPF;  
-        }  
-  
+            d2 = d2 + (12 - nCount) * digitoCPF;
+        }
+
         //Primeiro resto da divisão por 11.  
-        resto = (d1 % 11);  
-  
+        resto = (d1 % 11);
+
         //Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11 menos o resultado anterior.  
-        if (resto < 2) {  
-            digito1 = 0;  
-        } else {  
-            digito1 = 11 - resto;  
-        }  
-  
-        d2 += 2 * digito1;  
-  
+        if (resto < 2) {
+            digito1 = 0;
+        } else {
+            digito1 = 11 - resto;
+        }
+
+        d2 += 2 * digito1;
+
         //Segundo resto da divisão por 11.  
-        resto = (d2 % 11);  
-  
+        resto = (d2 % 11);
+
         //Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11 menos o resultado anterior.  
-        if (resto < 2) {  
-            digito2 = 0;  
-        } else {  
-            digito2 = 11 - resto;  
-        }  
-//Digito verificador do CPF que está sendo validado.  
-        String nDigVerific = strCpf.substring(strCpf.length() - 2, strCpf.length());  
-  
+        if (resto < 2) {
+            digito2 = 0;
+        } else {
+            digito2 = 11 - resto;
+        }
+        //Digito verificador do CPF que está sendo validado.  
+        String nDigVerific = strCpf.substring(strCpf.length() - 2, strCpf.length());
+
         //Concatenando o primeiro resto com o segundo.  
-        nDigResult = String.valueOf(digito1) + String.valueOf(digito2);  
-  
+        nDigResult = String.valueOf(digito1) + String.valueOf(digito2);
+
         //comparar o digito verificador do cpf com o primeiro resto + o segundo resto.  
-        return nDigVerific.equals(nDigResult);  
+        return nDigVerific.equals(nDigResult);
     }
     /* @MD5 Método de Encriptação da Senha em um HASH Hexadecimal */
+
     public static String md5(String senha) {
         MessageDigest md = null;
         try {
@@ -97,7 +98,7 @@ public class Util {
         return ((String) hash.toString(16));
     }
 
-    public boolean chkVazio(String... campos) {
+    public static boolean chkVazio(String... campos) {
         for (String chk : campos) {
             if (chk.isEmpty()) {
                 return false;
@@ -106,7 +107,7 @@ public class Util {
         return true;
     }
 
-    public boolean chkCaracteres(String... caracteres) {
+    public static boolean chkCaracteres(String... caracteres) {
         Pattern regex = Pattern.compile("\\W");
         Matcher comparador;
 
@@ -121,5 +122,4 @@ public class Util {
         }
         return true;
     }
-
 }
