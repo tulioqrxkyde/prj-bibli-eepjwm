dfea486df2ae5717a7df41e7abef9e4c82fd7a6b
pedrosaraiva@fvs.edu.br <pedrosaraiva@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2014-01-03 19:08:50 +0000

    teriminado cadastro de exemplares em acervo. Tem um problema de duplicidade nas listas será concertado em próximos comitters

---
+++ trunk/biblioteca-eeep/src/fvsosp/acervo/ExemplarAcervoTableModel.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fvsosp.acervo;
+
+import fvsosp.exemplar.Exemplar;
+import fvsosp.palavraschaves.PalavrasChaves;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Pedro Saraiva
+ */
+public class ExemplarAcervoTableModel extends AbstractTableModel {
+
+    private String[] nomeColunas = {"Tombo", "Nº Exemplar"};
+    private List<Exemplar> exemplares;
+
+    // construtor padrão criando um arraylist de alunos  
+    public ExemplarAcervoTableModel() {
+        exemplares = new ArrayList<Exemplar>();
+    }
+
+    // construtor que adiciona a lista passada pelo método ao alunos  
+    public ExemplarAcervoTableModel(List<Exemplar> lista) {
+        this();
+        this.exemplares.clear();
+        this.exemplares.addAll(lista);
+        super.fireTableDataChanged();
+    }
+
+    public int getRowCount() {
+        return exemplares.size();
+        //throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+    public int getColumnCount() {
+        return nomeColunas.length;
+        //throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        List<Exemplar> outraLitsa = (List<Exemplar>) exemplares;
+        Exemplar exemplar = outraLitsa.get(rowIndex);
+        DecimalFormat df = new DecimalFormat("0000000");
+        DecimalFormat df2 = new DecimalFormat("0000");
+        switch (columnIndex) {
+            case 0:
+                return df.format(exemplar.getTombo());
+            case 1:
+                return df2.format(exemplar.getExemplar());
+        }
+        return null;
+        //throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0:
+                return nomeColunas[0];
+            case 1:
+                return nomeColunas[1];
+        }
+        return null;
+    }
+}
