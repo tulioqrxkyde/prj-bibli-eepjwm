d537be1371f164cf38ea55312f0cf11d6b8aab99
adriano@fvs.edu.br <adriano@fvs.edu.br@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2014-02-08 14:46:58 +0000

    Termino da documentação de Editora

--- trunk/biblioteca-eeep/src/fvsosp/editora/EditoraRN.java
+++ trunk/biblioteca-eeep/src/fvsosp/editora/EditoraRN.java
@@ -24,6 +24,16 @@ public class EditoraRN {
 
     EditoraDAO dao = new EditoraDAO();
 
+    
+     /**
+     * Acessa o método dao.adicionar de EditoraDAO que por sua vez acessa o método
+     * da classe genérica (GenericDAO) salvando a Editora.
+     *
+     * @param ed Editora.
+     * @return booleano (true|false).
+     * @see EditoraDAO.
+     */
+    
     public boolean salvar(Editora ed) {
         if (ed.getIdEditora() == 0) {
             List<Editora> lista = dao.checkExists("nome", ed.getNome());
@@ -37,27 +47,76 @@ public class EditoraRN {
             return dao.atualizar(ed);
         }
     }
+    
+     /**
+     * Acessa o método dao.remover de EditoraDAO que por sua vez acessa o método
+     * da classe genérica (GenericDao) removendo a Editora.
+     *
+     * @param ed Editora.
+     * @return booleano (true|false).
+     * @see EditoraDAO.
+     */
 
     public boolean remover(Editora ed) {
         return dao.remover(ed);
 
     }
+    
+     /**
+     * Acessa o método dao.listar de EditoraDAO que por sua vez acessa o método da
+     * classe genérica (GenericDao) listando as Editoras.
+     *
+     * @return List(Editora).
+     * @see EditoraDAO.
+     */
 
     public List<Editora> listar() {
         return dao.listar();
     }
-
+    
+     /**
+     * Acessa o método dao.pesquisarNomeEditora(nome) de EditoraDAO.
+     *
+     * @param nome String.
+     * @return List(Editora).
+     * @see EditoraDAO.
+     */
     public List<Editora> pesquisarNomeEditora(String nome) {
         return dao.procuraNomeEditora(nome);
     }
     
+     /**
+     * Acessa o método dao.pesquisarNomeEditoraEq(nome) de EditoraDAO.
+     *
+     * @param nome String.
+     * @return Editora nome.
+     * @see EditoraDAO.
+     */
+    
     public Editora pesquisarNomeEditoraEq(String nome) {
         return dao.procuraNomeEditoraEq(nome);
     }
+    
+     /**
+     * Acessa o método dao.pesquisarAcervoEditora(ed) de EditoraDAO.
+     *
+     * @param ed String.
+     * @return Editora ed.
+     * @see EditoraDAO.
+     */
 
-    public List<Acervo> pesquisarAcervoEditora(Editora et) {
-        return dao.procuraAcervoEditora(et);
+    public List<Acervo> pesquisarAcervoEditora(Editora ed) {
+        return dao.procuraAcervoEditora(ed);
     }
+    
+        
+     /**
+     * Acessa o método dao.pesquisarCodigo(codigo) de EditoraDAO.
+     *
+     * @param codigo short.
+     * @return Editora editora.
+     * @see EditoraDAO.
+     */
 
     public Editora pesquisarCodigo(short codigo) {
         return dao.pesquisarCodigo(codigo);
