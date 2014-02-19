845a2a05b0e90dd4c17c91cccd99c1c5d8a5712c
junioraraujo.ads04@gmail.com <junioraraujo.ads04@gmail.com@ec16ae16-5d31-2fbb-ec0f-b340ac68fe6f>
2014-02-19 12:31:58 +0000

    Documentação Grupo Leitores.

--- trunk/biblioteca-eeep/src/fvsosp/grupoleitores/GruposLeitoresDAO.java
+++ trunk/biblioteca-eeep/src/fvsosp/grupoleitores/GruposLeitoresDAO.java
@@ -42,7 +42,7 @@ public class GruposLeitoresDAO extends GenericDAO<GruposLeitores> {
      * Pesquisa GrupoLeitores que contenham o nome passado por parâmetro.
      *
      * @param GrupoLeitores String.
-     * @return List(GrupodeLeitores) autores.
+     * @return List(GrupodeLeitores) descricaoPesquisa.
      */
     
     /* Metódo da Pesquisa criado por Marcos Ellys */
